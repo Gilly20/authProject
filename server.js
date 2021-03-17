c3a262e3598c850ce6ea49121b01c3568830a4de
Gilly20 <65635749+Gilly20@users.noreply.github.com>
2021-03-16 22:03:48 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { join } = require("path");
+const app = express();
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config.json");
+
+app.use(express.static(join(__dirname, "public")));
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    }),
+  
+    audience: authConfig.audience,
+    issuer: `https://${authConfig.domain}/`,
+    algorithms: ["RS256"]
+  });
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+
+app.get("/*", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.listen(3000, () => console.log("Application running on port 3000"));
+
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+      return res.status(401).send({ msg: "Invalid token" });
+    }
+  
+    next(err, req, res);
+  });
+
